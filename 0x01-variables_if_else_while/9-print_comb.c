fe11ae8dedd92c1221f6a20034ba515282ccaf19
ondanje <ondanje>
2023-02-17 12:29:03 -0800

    task 9 commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,17 @@
  */
 int main(void)
 {
-	int d1, d2;
+	int d1;
 
-	for (d1 = 0; d1 < 10; d1++)
+	for (d1 = 48; d1 < 58; d1++)
 	{
-		for (d2 = 0; d2 < 10; d2++)
+		putchar(d1);
+		if (d1 != 57)
 		{
-			putchar((d1 % 10) + '0');
-			putchar((d2 % 10) + '0');
-
-			if (d1 == 9 && d2 == 9)
-				continue;
-
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
