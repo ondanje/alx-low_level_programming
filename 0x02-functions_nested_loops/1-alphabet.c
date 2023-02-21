6aad855877455774fe119f96425052282bdf4a1c
ondanje <ondanje>
2023-02-21 06:26:13 -0800

    my second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * print_alphabet - printing the lower case
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int j = 0;
+
+	while (j <= 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		j++;
+		_putchar('\n');
+	}
+}
