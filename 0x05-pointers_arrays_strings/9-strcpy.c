4e641756fd3d508dd341e32e9b49084cc7f643b3
ondanje <ondanje>
2023-02-27 12:46:00 -0800

    my last commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	for (; src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
+	dest[i++] = '\0';
 	return (dest);
 }
