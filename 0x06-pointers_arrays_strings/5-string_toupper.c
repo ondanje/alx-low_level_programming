0eac02c02de07821a6d9e8c546f8d29cad449489
ondanje <ondanje>
2023-03-02 13:55:10 -0800

    my last commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * string_toupper-function that changes all lowercase to uppercase.
+ * @str: string to be converted
+ * Return: str
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+
+	return (str);
+}
+
