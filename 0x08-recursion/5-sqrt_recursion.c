a340003735888c70647f8dcce81cf5a21228f11a
ondanje <ondanje>
2023-03-08 11:31:14 -0800

    square root func.

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,6 @@
 #include"main.h"
+
+int actual_sqrt(int n, int i);
 /**
  * _sqrt_recursion-function that returns the natural square root.
  * @n: number(parameter)
@@ -7,12 +9,20 @@
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-		return (0);
-	}
-	if (n != floor(n))
-	{
 		return (-1);
-	}
-	return (_sqrt_recursion(n, 1));
+	return (actual_sqrt(n, 0));
+}
+/**
+ * actual_sqrt- function
+ * @n: function argument
+ * @i: function argument
+ * Return: 0
+ */
+int actual_sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt(n, i + 1));
 }
