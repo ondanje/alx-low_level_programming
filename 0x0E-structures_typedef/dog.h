3d28b68d81cd11fd1dd5aa56cd3c5bac9203fbdf
ondanje <ondanje>
2023-03-20 12:09:50 -0700

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type
+ * @name: name
+ * @age: age
+ * @owner: name of owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
