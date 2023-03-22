ebfc1851c87dce972b4431e072b7aa1748356ab3
ondanje <ondanje>
2023-03-22 12:11:56 -0700

    func print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,7 @@
  * print_name - entry point (func that prints name)
  * @name: parameter to be printed
  * @f: function pointer
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
