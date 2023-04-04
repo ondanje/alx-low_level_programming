0dd55923cd946c315e14d8fd8a08a06ad330da81
ondanje <cyrilondanje@gmail.com>
2023-04-04 02:59:37 -0700

    rees a listint_t list2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"lists.h"
+/**
+ * free_listint2-function that frees a listint_t list
+ * @head:argument(head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+	listint_t *current;
+
+
+	current = *head;
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+
+		current = next_node;
+	}
+	*head = NULL;
+}
