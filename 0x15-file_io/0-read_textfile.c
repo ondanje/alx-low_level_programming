1231cdd2d3a2830c088f86e8601b9b76a6124186
ondanje <cyrilondanje@gmail.com>
2023-04-10 13:06:25 -0700

    function that reads a text file and prints it to the POSIX stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *size;
-	FILE *fp;
+	ssize_t fp;
 	ssize_t pr, c;
 
 	if (filename == NULL)
@@ -16,34 +16,21 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	/*opening a file*/
-	fp = fopen(filename, "r");
-	if (fp == NULL)
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
 	{
 		return (0);
 	}
 	size = (char *) malloc(letters * sizeof(char));
-	if (size == NULL)
-	{
-		fclose(fp);
-		return (0);
-	}
+	
 	/*Reading from fp, space allocated and letters-what to read*/
-	pr = fread(size, sizeof(char), letters, fp);
-	if (pr == 0)
-	{
-		free(size);
-		fclose(fp);
-	}
-	c = fwrite(size, sizeof(char), pr, stdout);
-	if (c != pr)
-	{
-		free(size);
-		fclose(fp);
-		return (0);
-	}
+	pr = read(fp, size, letters);
+
+	c = write(STDOUT_FILENO, size, pr);
+
 	free(size);
 
-	fclose(fp);
+	close(fp);
 
 	return (c);
 }
