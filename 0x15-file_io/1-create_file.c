d7a65fb6b7da790ec441610dacdba8f5e53b5059
ondanje <cyrilondanje@gmail.com>
2023-04-10 13:31:16 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,6 @@
 int create_file(const char *filename, char *text_content)
 {
 	FILE *fp;
-	int i;
 	int len;
 	int content;
 
@@ -26,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i = 0; i != '\0'; i++)
+		for (len = 0; text_content[len];)
 		{
 			len++;
 		}
