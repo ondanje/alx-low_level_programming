8eea983118b1ac94ba3ffa4cfb164e86e07cb863
ondanje <cyrilondanje@gmail.com>
2023-04-11 03:50:02 -0700

    function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * get_endianness-function that checks the endianness
+ * Return:0
+ */
+int get_endianness(void)
+{
+	unsigned int x = 1;
+
+	char *c = (char *) &x;
+
+	return ((int)*c);
+}
