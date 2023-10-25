e916fbc5a155c096855da93390c9206fd1e24d67
ondanje <cyrilondanje@gmail.com>
2023-10-25 20:56:41 +0300

    interview quiz

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Technical quiz
+"""
+
+
+def island_perimeter(grid):
+    """
+    function to calculate perimeter of a grid
+    """
+    if not grid:
+        return 0
+
+    rows, cols = len(grid), len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
